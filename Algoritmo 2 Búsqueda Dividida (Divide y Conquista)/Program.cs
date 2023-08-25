dd891903079b3a64e4a702ac0adb24a3a5daba43
JoseFabela <jfabela522@gmail.com>
2023-08-25 01:02:25 -0600

    Agregar archivos de proyecto.

---
+++ Algoritmo 2 Búsqueda Dividida (Divide y Conquista)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmo_2_Búsqueda_Dividida__Divide_y_Conquista_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 5, 12, 9, 20, 3, 15, 8 };
+
+            int maxNumber = FindMaxDivideAndConquer(numbers, 0, numbers.Count - 1);
+
+            Console.WriteLine($"The maximum number in the list is: {maxNumber}");
+            Console.ReadLine();
+             }
+        static int FindMaxDivideAndConquer(List<int> numbers, int low, int high)
+        {
+            if (low == high)
+                return numbers[low];
+
+            int mid = (low + high) / 2;
+            int leftMax = FindMaxDivideAndConquer(numbers, low, mid);
+            int rightMax = FindMaxDivideAndConquer(numbers, mid + 1, high);
+
+            return Math.Max(leftMax, rightMax);
+        }
+    }
+}
